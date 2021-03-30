dc64efb5458fbe72e7b315e3b02049c19b5fce32
Minhao Zhou D75Z <SE2321000016-D75Z@sll.se>
2021-03-30 11:34:37 +0200

    Fix bug

--- fhk_main_script.js
+++ fhk_main_script.js
@@ -110,8 +110,8 @@ window.addEventListener('vaReportComponents.loaded', function () {
             iframe_title_div.innerHTML = iframe_title_div_innerHTML;
             document.body.appendChild(iframe_title_div);
             html2canvas(iframe_title_div, {
-                width: 500,
-                height: 100,
+                width: 700,
+                height: 200,
                 scale: 1.5,
                 backgroundColor: null
             }).then(function (title_canvas) {
