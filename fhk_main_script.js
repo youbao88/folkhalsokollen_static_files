df1f3fe558b47d158abfe6a43e07a9ce4c5a7892
Minhao Zhou D75Z <SE2321000016-D75Z@sll.se>
2021-04-06 13:44:14 +0200

    Fix bug

--- fhk_main_script.js
+++ fhk_main_script.js
@@ -111,7 +111,7 @@ window.addEventListener('vaReportComponents.loaded', function () {
             } else if (currentIndicator != newIndicator) {
                 sasReport.getReportHandle().then(reportHandle => {
                     var parameters = indicator_name_parameter_map[newIndicator];
-                    reportHandle.updateReportParameters(parameters);
+                    reportHandle.setReportParameters(parameters);
                 });
                 currentIndicator = newIndicator;
             }
