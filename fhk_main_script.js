3628e6ae0fabd71dcdefb91a8c44537b8ec805ae
Minhao Zhou D75Z <SE2321000016-D75Z@sll.se>
2021-04-13 16:41:12 +0200

    Update custom loading screen

--- fhk_main_script.js
+++ fhk_main_script.js
@@ -6,6 +6,146 @@ var styles = `
     .sas_components-Pane-Pane_pane {
         --pane-bg: transparent !important;
     }
+            .lds-ring {
+            display: block;
+            width: 80px;
+            height: 80px;
+            margin: auto
+        }
+
+        .lds-ring div {
+            box-sizing: border-box;
+            display: block;
+            position: absolute;
+            width: 64px;
+            height: 64px;
+            margin: 8px;
+            border: 8px solid #612361;
+            border-radius: 50%;
+            animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+            border-color: #612361 transparent transparent transparent;
+        }
+
+        .lds-ring div:nth-child(1) {
+            animation-delay: -0.45s;
+        }
+
+        .lds-ring div:nth-child(2) {
+            animation-delay: -0.3s;
+        }
+
+        .lds-ring div:nth-child(3) {
+            animation-delay: -0.15s;
+        }
+
+        @keyframes lds-ring {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+
+        .animation-box {
+            width: 75%;
+            height: 30rem;
+            background-color: darkblue;
+            margin: 0 auto;
+            overflow: hidden;
+            position: relative;
+        }
+
+        @keyframes fadeInOut {
+            0% {
+                opacity: 0;
+            }
+
+            2% {
+                opacity: 0;
+            }
+
+            5% {
+                opacity: 1;
+            }
+
+            17% {
+                opacity: 1;
+            }
+
+            19% {
+                opacity: 1;
+            }
+
+            24% {
+                opacity: 0;
+            }
+
+            80% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 0;
+            }
+        }
+
+        .animation-box h1 {
+            position: absolute;
+            left: 5%;
+            top: 0;
+            font-size: 4em;
+            color: white;
+            font-weight: 400;
+        }
+
+        .first-line,
+        .second-line,
+        .third-line,
+        .fourth-line {
+            font-size: 1.5rem;
+            font-family: Arial, Helvetica, sans-serif;
+            position: absolute;
+            opacity: 0;
+            animation-name: fadeInOut;
+            animation-iteration-count: infinite;
+            width: 100%;
+            text-align: center;
+            margin-top: 50px;
+        }
+
+        .first-line {
+            animation-duration: 32s;
+        }
+
+        .second-line {
+            animation-delay: 8s;
+            animation-duration: 32s;
+        }
+
+        .third-line {
+            animation-delay: 16s;
+            animation-duration: 32s;
+        }
+
+        .fourth-line {
+            animation-delay: 24s;
+            animation-duration: 32s;
+        }
+
+        .loading-text{
+            text-align: center;
+            display: block;
+            margin: auto;
+            font-size: 1.1rem;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+
+        .loading-tip {
+            display: block;
+            margin: auto;
+        }
 `;
 var styleSheet = document.createElement("style");
 styleSheet.type = "text/css";
@@ -82,6 +222,45 @@ function disableScrollDoubleClickOutline() {
 }
 window.addEventListener('vaReportComponents.loaded', function () {
     var sasReport = document.getElementById("my-report");
+
+    var observer = new MutationObserver(mutationRecords => {
+        console.log(mutationRecords)
+        var sasLoadingDiv = document.getElementsByClassName('sas_components-BusyStateIndicator-BusyStateIndicator_state');
+        if (sasLoadingDiv){
+            sasLoadingDiv[0].style.visibility = "hidden";
+            var sasMainPanel = document.getElementsByClassName('sas_components-Pane-Pane_pane')[0];
+            var customLoadingDiv = sasMainPanel.appendChild(document.createElement('div'));
+            customLoadingDiv.setAttribute('id', 'customLoadingDiv');
+            customLoadingDiv.innerHTML = `
+                <div class='loading screen'>
+                    <div class="lds-ring">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class='loading-text'>Läser in...</div>
+                    <div class='loading-tip'>
+                        <p class="first-line">This is your last chance. After this, there is no turning back.</p>
+
+                        <p class="second-line">You take the blue pill - the story ends, you wake up in your bed and believe
+                            whatever you want to believe.</p>
+
+                        <p class="third-line">You take the red pill - you stay in Wonderland and I show you how deep the
+                            rabbit-hole goes.</p>
+                        <p class="fourth-line">Remember: all I'm offering is the truth. Nothing more.</p>
+                    </div>
+                </div>
+            `
+            observer.disconnect();
+        }
+    })
+
+    observer.observe(sasReport, {
+        childList: true,
+        subtree: true
+    });
+
     //disable the right-click function
     sasReport.addEventListener("contextmenu", function (event) {
         event.stopPropagation();
