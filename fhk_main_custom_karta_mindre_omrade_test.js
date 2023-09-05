c896af9de05ccf24a2af459ffabf179ec337a13f
Minhao Zhou D75Z <SE2321000016-D75Z@sll.se>
2023-09-05 14:21:22 +0200

    Update the copy text

--- fhk_main_custom_karta_mindre_omrade_test.js
+++ fhk_main_custom_karta_mindre_omrade_test.js
@@ -12,7 +12,7 @@ function copyUrl() {
   navigator.clipboard.writeText(copyText.value);
   let copyButton = document.getElementById("copyButton");
   copyButton.style.backgroundColor = "#5cb85c";
-  copyButton.innerHTML = `<div><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg> Kopierade</div>`;
+  copyButton.innerHTML = `<div><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg> Kopierad till urklipp</div>`;
 }
 document.addEventListener("DOMContentLoaded", function (event) {
   //Disable animation and set the background of loading page to transparent
